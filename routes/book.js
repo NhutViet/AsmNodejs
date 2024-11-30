e098f43316c1d8c8eafe5134975fbc2d8c97ab3a
NhutViet <156928605+NhutViet@users.noreply.github.com>
2024-11-30 08:12:07 +0700

    update

--- routes/book.js
+++ routes/book.js
@@ -58,14 +58,25 @@ router.get("/gia", async function (req, res) {
 //lấy danh sách từ 60000 => 100000
 // http://localhost:3000/books/giaMaxMin?min=60000&max=100000
 router.get("/giaMaxMin", async function (req, res) {
-  const token = req.header("Authorization").split(" ")[1];
-  if (token) {
-    JWT.verify(token, config.SECRETKEY, async function (err, id) {});
-  }
   try {
-    const { max, min } = req.query;
-    const data = await bookModel.find({ giaTien: { $gt: min, $lt: max } });
-    res.status(200).json(data);
+    const token = req.header("Authorization").split(" ")[1];
+    if (token) {
+      JWT.verify(token, config.SECRETKEY, async function (err, id) {
+        if (err) {
+          res
+            .status(403)
+            .json({ status: false, message: "lỗi xảy ra, token hết hạn" });
+        } else {
+          const { max, min } = req.query;
+          const data = await bookModel.find({
+            giaTien: { $gt: min, $lt: max },
+          });
+          res.status(200).json(data);
+        }
+      });
+    } else {
+      res.status(401).json({ status: false, message: "không xác thực" });
+    }
   } catch (e) {
     res.status(400).json({ status: false, message: e });
   }
@@ -75,9 +86,22 @@ router.get("/giaMaxMin", async function (req, res) {
 // http://localhost:3000/books/NXB/NXBTPHCM
 router.get("/NXB/:nhaXuatBan", async function (req, res) {
   try {
-    const { nhaXuatBan } = req.params;
-    const data = await bookModel.find({ nhaXuatBan: nhaXuatBan });
-    res.status(200).json(data);
+    const token = req.header("Authorization").split(" ")[1];
+    if (token) {
+      JWT.verify(token, config.SECRETKEY, async function (err, id) {
+        if (err) {
+          res
+            .status(403)
+            .json({ status: false, message: "lỗi xảy ra, token hết hạn" });
+        } else {
+          const { nhaXuatBan } = req.params;
+          const data = await bookModel.find({ nhaXuatBan: nhaXuatBan });
+          res.status(200).json(data);
+        }
+      });
+    } else {
+      res.status(401).json({ status: false, message: "không xác thực" });
+    }
   } catch (e) {
     res.status(400).json({ status: false, message: e });
   }
@@ -87,9 +111,22 @@ router.get("/NXB/:nhaXuatBan", async function (req, res) {
 // http://localhost:3000/books/chitiet/ nhập id cần tìm
 router.get("/chitiet/:id", async function (req, res) {
   try {
-    const { id } = req.params;
-    const data = await bookModel.findById(id);
-    res.status(200).json(data);
+    const token = req.header("Authorization").split(" ")[1];
+    if (token) {
+      JWT.verify(token, config.SECRETKEY, async function (err, id) {
+        if (err) {
+          res
+            .status(403)
+            .json({ status: false, message: "lỗi xảy ra, token hết hạn" });
+        } else {
+          const { id } = req.params;
+          const data = await bookModel.findById(id);
+          res.status(200).json(data);
+        }
+      });
+    } else {
+      res.status(401).json({ status: false, message: "không xác thực" });
+    }
   } catch (e) {
     res.status(400).json({ status: false, message: e });
   }
@@ -99,11 +136,25 @@ router.get("/chitiet/:id", async function (req, res) {
 // http://localhost:3000/books/NXBGiaTien?nhaXuatBan=NXBTPHCM&giaTien=100000
 router.get("/NXBGiaTien", async function (req, res) {
   try {
-    const { nhaXuatBan, giaTien } = req.query;
-    const data = await bookModel.find({
-      $and: [{ nhaXuatBan: nhaXuatBan }, { giaTien: { $gt: giaTien } }],
-    });
-    res.status(200).json(data);
+    const token = req.header("Authorization").split(" ")[1];
+
+    if (token) {
+      JWT.verify(token, config.SECRETKEY, async function (err, id) {
+        if (err) {
+          res
+            .status(403)
+            .json({ status: false, message: "lỗi xảy ra, token hết hạn" });
+        } else {
+          const { nhaXuatBan, giaTien } = req.query;
+          const data = await bookModel.find({
+            $and: [{ nhaXuatBan: nhaXuatBan }, { giaTien: { $gt: giaTien } }],
+          });
+          res.status(200).json(data);
+        }
+      });
+    } else {
+      res.status(401).json({ status: false, message: "không xác thực" });
+    }
   } catch (e) {
     res.status(400).json({ status: false, message: e });
   }
@@ -113,8 +164,22 @@ router.get("/NXBGiaTien", async function (req, res) {
 // http://localhost:3000/books/giamDan
 router.get("/giamDan", async function (req, res) {
   try {
-    const data = await bookModel.find().sort({ giaTien: -1 });
-    res.status(400).json(data);
+    const token = req.header("Authorization").split(" ")[1];
+
+    if (token) {
+      JWT.verify(token, config.SECRETKEY, async function (err, id) {
+        if (err) {
+          res
+            .status(403)
+            .json({ status: false, message: "lỗi xảy ra, token hết hạn" });
+        } else {
+          const data = await bookModel.find().sort({ giaTien: -1 });
+          res.status(400).json(data);
+        }
+      });
+    } else {
+      res.status(401).json({ status: false, message: "không xác thực" });
+    }
   } catch (e) {
     res.status(400).json({ status: false, message: e });
   }
@@ -124,10 +189,25 @@ router.get("/giamDan", async function (req, res) {
 // http://localhost:3000/books/add
 router.post("/add", async function (req, res) {
   try {
-    const { tuaDeSach, tacGia, nhaXuatBan, giaTien } = req.body;
-    const newBook = { tuaDeSach, tacGia, nhaXuatBan, giaTien };
-    await bookModel.create(newBook);
-    res.status(200).json({ status: true, message: "Thêm Book thành công" });
+    const token = req.header("Authorization").split(" ")[1];
+    if (token) {
+      JWT.verify(token, config.SECRETKEY, async function (err, id) {
+        if (err) {
+          res
+            .status(403)
+            .json({ status: false, message: "lỗi xảy ra, token hết hạn" });
+        } else {
+          const { tuaDeSach, tacGia, nhaXuatBan, giaTien } = req.body;
+          const newBook = { tuaDeSach, tacGia, nhaXuatBan, giaTien };
+          await bookModel.create(newBook);
+          res
+            .status(200)
+            .json({ status: true, message: "Thêm Book thành công" });
+        }
+      });
+    } else {
+      res.status(401).json({ status: false, message: "không xác thực" });
+    }
   } catch (e) {
     res.status(400).json({ message: e });
   }
@@ -137,12 +217,28 @@ router.post("/add", async function (req, res) {
 // http://localhost:3000/books/delete/Nhập id cần xoá
 router.delete("/delete/:id", async function (req, res) {
   try {
-    const { id } = req.params;
-    const findBook = await bookModel.findByIdAndDelete(id);
-    if (findBook) {
-      res.status(200).json({ status: true, message: "Xoá Thành công" });
+    const token = req.header("Authorization").split(" ")[1];
+
+    if (token) {
+      JWT.verify(token, config.SECRETKEY, async function (err, id) {
+        if (err) {
+          res
+            .status(403)
+            .json({ status: false, message: "lỗi xảy ra, token hết hạn" });
+        } else {
+          const { id } = req.params;
+          const findBook = await bookModel.findByIdAndDelete(id);
+          if (findBook) {
+            res.status(200).json({ status: true, message: "Xoá Thành công" });
+          } else {
+            res
+              .status(404)
+              .json({ status: false, message: "Không tìm thấy ID" });
+          }
+        }
+      });
     } else {
-      res.status(404).json({ status: false, message: "Không tìm thấy ID" });
+      res.status(401).json({ status: false, message: "không xác thực" });
     }
   } catch (e) {
     res.status(400).json({ message: e });
@@ -153,19 +249,34 @@ router.delete("/delete/:id", async function (req, res) {
 // http://localhost:3000/books/edit
 router.put("/edit", async function (req, res) {
   try {
-    const { id, tuaDeSach, tacGia, nhaXuatBan, giaTien } = req.body;
-    const findBook = await bookModel.findById(id);
+    const token = req.header("Authorization").split(" ")[1];
+    if (token) {
+      JWT.verify(token, config.SECRETKEY, async function (err, id) {
+        if (err) {
+          res
+            .status(403)
+            .json({ status: false, message: "lỗi xảy ra, token hết hạn" });
+        } else {
+          const { id, tuaDeSach, tacGia, nhaXuatBan, giaTien } = req.body;
+          const findBook = await bookModel.findById(id);
 
-    if (findBook) {
-      findBook.tuaDeSach = tuaDeSach ? tuaDeSach : findBook.tuaDeSach;
-      findBook.tacGia = tacGia ? tacGia : findBook.tacGia;
-      findBook.nhaXuatBan = nhaXuatBan ? nhaXuatBan : findBook.nhaXuatBan;
-      findBook.giaTien = giaTien ? giaTien : findBook.giaTien;
+          if (findBook) {
+            findBook.tuaDeSach = tuaDeSach ? tuaDeSach : findBook.tuaDeSach;
+            findBook.tacGia = tacGia ? tacGia : findBook.tacGia;
+            findBook.nhaXuatBan = nhaXuatBan ? nhaXuatBan : findBook.nhaXuatBan;
+            findBook.giaTien = giaTien ? giaTien : findBook.giaTien;
 
-      await findBook.save();
-      res.status(200).json({ status: true, message: "Sửa thành công" });
+            await findBook.save();
+            res.status(200).json({ status: true, message: "Sửa thành công" });
+          } else {
+            res
+              .status(404)
+              .json({ status: true, message: "Không tìm thấy ID" });
+          }
+        }
+      });
     } else {
-      res.status(404).json({ status: true, message: "Không tìm thấy ID" });
+      res.status(401).json({ status: false, message: "không xác thực" });
     }
   } catch (e) {
     res.status(400).json({ message: e });
